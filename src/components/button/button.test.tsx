e0aab62435f0403ed0670dc443dd2aafcfb1711f
Sonny <sonnyasdev@gmail.com>
2026-01-30 23:47:51 +0100

    ci: try new fix for publish

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,3 +1,4 @@
+import '@testing-library/jest-dom/vitest';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 import { Button } from './button';
