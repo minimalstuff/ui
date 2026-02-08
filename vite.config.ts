1fa9df7fb9702b9c9fa0a516ce06f88dbd10b833
Sonny <sonnyasdev@gmail.com>
2026-02-08 18:22:33 +0100

    chore(deps): update deps and replace plugin-react-swc with plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest/config" />
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { playwright } from '@vitest/browser-playwright';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
