48b7863f686ed1da92fdd58880da1667cc6d1eb7
Sonny <sonnyasdev@gmail.com>
2026-05-01 20:21:17 +0200

    chore: fix OXC conf

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'oxlint';
 import { minimalstuffPreset } from '@minimalstuff/tooling/oxc/lint';
 
 export default defineConfig({
-	extends: [minimalstuffPreset()],
+	extends: [minimalstuffPreset({ react: true })],
 });
