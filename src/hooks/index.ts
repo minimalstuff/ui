57a266036b74e44c226b3bd93c1fadc7fac08786
Sonny <sonnyasdev@gmail.com>
2026-08-14 03:59:16 +0200

    refactor(hooks): remove the unused useRunAfterAnimation hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useDisableHotkeysWhileMounted } from './use_disable_hotkeys_while_mounted/use_disable_hotkeys_while_mounted';
 export { useIsClient } from './use_is_client/use_is_client';
 export { withClientOnly } from './use_is_client/with_client_only';
-export { useRunAfterAnimation } from './use_run_after_animation/use_run_after_animation';
